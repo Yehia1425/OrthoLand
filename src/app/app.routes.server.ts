e4ea43a8a11614d20df391919d6df9dc190344a6
Yehiea Mostafa <yehieamostafa10@gmail.com>
2026-03-12 01:07:06 +0200

    second

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,53 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'categories',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'categories-offers',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'checkout',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+
+  // dynamic routes
+  {
+    path: 'products/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'product-details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'offers/:id',
+    renderMode: RenderMode.Server
+  },
+
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
+
 ];
